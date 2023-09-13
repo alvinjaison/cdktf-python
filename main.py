181ffe49798c2e2bc882501434db733787b65fc2
Alvin Jaison <alvinjaison@outlook.com>
2023-09-13 17:00:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-#!/usr/bin/env python
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 from constructs import Construct
 from cdktf import App, TerraformStack
+from imports.aws.provider import AwsProvider
+from imports.aws.sns_topic import SnsTopic
+from imports.terraform_aws_modules.aws import Vpc
+from imports.aws.lambda_function import LambdaFunction
+from imports.aws.iam_role import IamRole
 
 
 class MyStack(TerraformStack):
-    def __init__(self, scope: Construct, id: str):
-        super().__init__(scope, id)
+    def __init__(self, scope: Construct, ns: str):
+        super().__init__(scope, ns)
 
-        # define resources here
+        AwsProvider(self, 'Aws', region='eu-central-1')
+
+        Vpc(self, 'CustomVpc',
+            name='custom-vpc',
+            cidr='10.0.0.0/16',
+            azs=["us-east-1a", "us-east-1b"],
+            public_subnets=["10.0.1.0/24", "10.0.2.0/24"]
+            )
+        SnsTopic(self, 'Topic', display_name='my-first-sns-topic')
+        role = IamRole(self, 'Role', name='lambda-role',
+                       assume_role_policy='{}')
+        LambdaFunction(self, 'Lambda', function_name='my-first-lambda-function',
+                       role=role.arn, handler='index.handler', runtime='python3.6')
 
 
 app = App()
-MyStack(app, "cdktf-python")
+MyStack(app, "python-aws")
 
 app.synth()
